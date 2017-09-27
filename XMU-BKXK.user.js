ea66667f14f45a5cdc90ab6d5245360f42e87354
linjinzhen <linjinzhen@hotmail.com>
2017-09-27 21:20:19 +0800

    1.修改无法读取全局选项的bug；2.一些小修正；3.版本升级

--- XMU-BKXK.user.js
+++ XMU-BKXK.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         XMU-BKXK
 // @namespace    undefined
-// @version      0.1.0
+// @version      0.1.1
 // @description  厦门大学本科选课系统改造脚本
 // @author       linjinzhen
 // @match        http://bkxk.xmu.edu.cn/*
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 (function() {
-  if (!$("#show_rs_global, #show_ym_global, #show_ct_global, #show_yx_global, #show_zc_global").length) {
+  if (!$("#show_rs_global, #show_ym_global, #show_ct_global, #show_yx_global, #show_zc_global", top.document).length) {
     $("head", top.document).after('<input type="hidden" id="show_zc_global">');
     $("head", top.document).after('<input type="hidden" id="show_rs_global">');
     $("head", top.document).after('<input type="hidden" id="show_ym_global">');
@@ -22,10 +22,13 @@
     $("#show_ct_global", top.document).attr("checked", true);
     $("#show_yx_global", top.document).attr("checked", false);
   }
+
   var base_url = "https://gitee.com/linjinzhen/xmu-bkxk/raw/master/js/";
-  jQuery("script[src$=ggk\\.js]").after('<script type="text/javascript" src="' + base_url + 'ggk.js"></script>');
-  jQuery("script[src$=yxxx\\.js]").after('<script type="text/javascript" src="' + base_url + 'yxxx.js"></script>');
-  jQuery("script[src$=qxxbxk\\.js]").after('<script type="text/javascript" src="' + base_url + 'qxxbxk.js"></script>');
-  jQuery("script[src$=yxbxk\\.js]").after('<script type="text/javascript" src="' + base_url + 'yxbxk.js"></script>');
-  //jQuery("script[src$=qxxxx\\.js]").after('<script type="text/javascript" src="' + base_url + 'qxxxx.js"></script>');
+  //var base_url = "http://127.0.0.1:12347/js/";
+
+  $("script[src$=ggk\\.js]").after('<script type="text/javascript" src="' + base_url + 'ggk.js"></script>');
+  $("script[src$=yxxx\\.js]").after('<script type="text/javascript" src="' + base_url + 'yxxx.js"></script>');
+  $("script[src$=qxxbxk\\.js]").after('<script type="text/javascript" src="' + base_url + 'qxxbxk.js"></script>');
+  $("script[src$=yxbxk\\.js]").after('<script type="text/javascript" src="' + base_url + 'yxbxk.js"></script>');
+  //$("script[src$=qxxxx\\.js]").after('<script type="text/javascript" src="' + base_url + 'qxxxx.js"></script>');
 })();
